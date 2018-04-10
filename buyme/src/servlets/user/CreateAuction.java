642672ca6f98b3db708ff4538e898573f6d53a63
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-04-10 10:57:47 -0400

    progress on create auction

--- buyme/src/servlets/user/CreateAuction.java
+++ buyme/src/servlets/user/CreateAuction.java
@@ -1,11 +1,21 @@
 package servlets.user;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.fabric.xmlrpc.base.Array;
+
+import main.Database;
 
 @WebServlet("/user/CreateAuction")
 public class CreateAuction extends HttpServlet {
@@ -17,6 +27,47 @@ public class CreateAuction extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String[] staticParameters = {
+				"category",
+				"subcategory",
+				"initialPrice",
+				"bidIncrement",
+				"minimumPrice",
+				"closeTime"
+		};
+		
+		String category = request.getParameter("category");
+		String subcategory = request.getParameter("subcategory");
+		String initialPrice = request.getParameter("initialPrice");
+		String bidIncrement = request.getParameter("bidIncrement");
+		String minimumPrice = request.getParameter("minimumPrice");
+		String closeTime = request.getParameter("closeTime");
+//		
+//		Enumeration<String> parameters = request.getParameterNames();
+//		
+//		System.out.println("This is the test");
+//		while (parameters.hasMoreElements()) {
+//			if (staticParameters.)
+//			System.out.println(parameters.nextElement());
+//		}
+		
+		try {
+			
+			Database db = new Database();
+			Connection con = db.connect();
+			ResultSet rs = con.createStatement().executeQuery("SELECT name FROM CategoryField WHERE category='" + category + "' AND (subcategory IS NULL OR subcategory='" + subcategory + "');");
+			
+			while (rs.next()) {
+				
+				String fieldName = rs.getString("name");
+				String fieldValue = request.getParameter(fieldName);
+				
+				System.out.println(fieldName + ": " + fieldValue);
+			}
+			
+		} catch (ClassNotFoundException | SQLException e) {
+
+		}
 	}
 
 }
