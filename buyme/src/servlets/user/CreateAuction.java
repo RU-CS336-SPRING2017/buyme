b65eb4ab7dcaa673d5064ae30bb1db971d6e67de
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-04-10 23:56:36 -0400

    transaction works

--- buyme/src/servlets/user/CreateAuction.java
+++ buyme/src/servlets/user/CreateAuction.java
@@ -4,16 +4,11 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.mysql.fabric.xmlrpc.base.Array;
 
 import main.Database;
 
@@ -27,15 +22,6 @@ public class CreateAuction extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String[] staticParameters = {
-				"category",
-				"subcategory",
-				"initialPrice",
-				"bidIncrement",
-				"minimumPrice",
-				"closeTime"
-		};
-		
 		String category = request.getParameter("category");
 		String subcategory = request.getParameter("subcategory");
 		String initialPrice = request.getParameter("initialPrice");
@@ -43,45 +29,41 @@ public class CreateAuction extends HttpServlet {
 		String minimumPrice = request.getParameter("minimumPrice");
 		String closeTime = request.getParameter("closeTime");
 		String auctioneer = request.getUserPrincipal().getName();
-//		
-//		Enumeration<String> parameters = request.getParameterNames();
-//		
-//		System.out.println("This is the test");
-//		while (parameters.hasMoreElements()) {
-//			if (staticParameters.)
-//			System.out.println(parameters.nextElement());
-//		}
+		
+		Connection con;
 		
 		try {
 			
 			Database db = new Database();
-			Connection con = db.connect();
+			con = db.connect();
+			
+			con.setAutoCommit(false);
+			con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			
+			
 			ResultSet rs = con.createStatement().executeQuery("SELECT name FROM CategoryField WHERE category='" + category + "' AND (subcategory IS NULL OR subcategory='" + subcategory + "');");
 			
-			String createAuction =
-					"START TRANSACTION; \n" + 
+			con.createStatement().execute(
 					"INSERT INTO Auction (openTime, closeTime, initialPrice, bidIncrement, auctioneer, subcategory, category) \n" +
-					"VALUES ('2018-03-03T10:24', '" + closeTime + "', " + initialPrice + ", " + bidIncrement + ", '" + auctioneer + "', '" + subcategory + "', '" + category + "'); \n";
+					"VALUES ('2018-03-03T10:24', '" + closeTime + "', " + initialPrice + ", " + bidIncrement + ", '" + auctioneer + "', '" + subcategory + "', '" + category + "');"
+			);
 			
 			while (rs.next()) {
 				
 				String fieldName = rs.getString("name");
 				String fieldValue = request.getParameter(fieldName);
 				
-				createAuction += 
+				con.createStatement().execute( 
 						"INSERT INTO AuctionField (auction, field, category, value) \n" +
-						"VALUES (LAST_INSERT_ID(), '" + fieldName + "', '" + category + "', '" + fieldValue + "'); \n";
+						"VALUES (LAST_INSERT_ID(), '" + fieldName + "', '" + category + "', '" + fieldValue + "');"
+				);
 			}
 			
-			createAuction += "COMMIT;";
-			System.out.println(createAuction);
-			con.createStatement().execute(createAuction);
+			con.commit();
 			con.close();
-			System.out.println("Worked!");
 			
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
