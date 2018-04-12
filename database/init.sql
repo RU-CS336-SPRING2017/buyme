26e04cff59c56936c995c907779f49858c61670f
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-04-12 19:07:38 -0400

    added other users

--- database/init.sql
+++ database/init.sql
@@ -3,6 +3,15 @@ USE BuyMe;
 INSERT INTO Account (username, password, type)
 VALUES ('admin', 'admin', 'admin');
 
+INSERT INTO Account (username, password, type)
+VALUES ('rep', 'rep', 'customerRep');
+
+INSERT INTO Account (username, password, type)
+VALUES ('fun', 'time', 'user');
+
+INSERT INTO Account (username, password, type)
+VALUES ('hey', 'there', 'user');
+
 INSERT INTO ItemCategory (name)
 VALUES ('Drank');
 
