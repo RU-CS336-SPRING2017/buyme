002246a80d5aaff5c9485351a09999ccecbfab5a
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-04-22 10:27:30 -0400

    added users and questions

--- database/init.sql
+++ database/init.sql
@@ -13,6 +13,12 @@ VALUES ('Rep1', 'buyme1', 'customerRep');
 INSERT INTO Account (username, password, type)
 VALUES ('User1', 'pass18', 'user');
 
+INSERT INTO Account (username, password, type)
+VALUES ('User2', 'pass18', 'user');
+
+INSERT INTO Account (username, password, type)
+VALUES ('User3', 'pass18', 'user');
+
 -- Adding categories
 
 INSERT INTO ItemCategory (name)
@@ -43,5 +49,16 @@ VALUES ('Screen Size', 'Computers', 'Tablets');
 
 INSERT INTO CategoryField (name, category, subcategory)
 VALUES ('Power Supply', 'Computers', 'Desktops');
+
+-- Adding Questions
+
+INSERT INTO Question (question,answer)
+VALUES ('How do I pay my taxes?', 'You dont man.');
+
+INSERT INTO Question (question,answer)
+VALUES ('Do you guys sell food?', 'We do not sell anything, please messages sellers directly for info on what they sell.');
+
+INSERT INTO Question (question,answer)
+VALUES ('How do I send messages?', 'Go into Messages in the navbar and click the compose button.');
 
 COMMIT;
