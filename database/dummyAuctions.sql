dbd99cfcabd597af6571f1bb478f1e1f436438b7
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-04-22 11:18:36 -0400

    fixed id

--- database/dummyAuctions.sql
+++ database/dummyAuctions.sql
@@ -3,7 +3,7 @@
 
 USE BuyMe;
 
-CREATE TRANSACTION;
+START TRANSACTION;
 INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
 VALUES (TIMESTAMPADD(WEEK,20,NOW()),10,20,'This is a dummy auction.','User2','Laptops','Computers','D Dummy Laptop 1 - The Sky is Blue',2001);
 INSERT INTO AuctionField (auction,field,category,value)
@@ -14,100 +14,100 @@ INSERT INTO AuctionField (auction,field,category,value)
 VALUES (2001,'Memory','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,7,NOW()),10,7,'This is a dummy auction.','User3','Desktops','Computers','H Dummy Desktop 1 - We the People');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,7,NOW()),10,7,'This is a dummy auction.','User3','Desktops','Computers','H Dummy Desktop 1 - We the People',2002);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (2002,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2002,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2002,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Power Supply','Computers','1');
+VALUES (2002,'Power Supply','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,8,NOW()),10,8,'This is a dummy auction.','User1','Tablets','Computers','G Dummy Tablet 1 - How Are You');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,8,NOW()),10,8,'This is a dummy auction.','User1','Tablets','Computers','G Dummy Tablet 1 - How Are You',2003);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (2003,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2003,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2003,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Screen Size','Computers','1');
+VALUES (2003,'Screen Size','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,5,NOW()),10,5,'This is a dummy auction.','User1','Laptops','Computers','A Dummy Laptop 2 - Anything New');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,5,NOW()),10,5,'This is a dummy auction.','User1','Laptops','Computers','A Dummy Laptop 2 - Anything New',2004);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (2004,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2004,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2004,'Memory','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,3,NOW()),10,3,'This is a dummy auction.','User3','Desktops','Computers','F Dummy Desktop 2 - Ten Earths');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,3,NOW()),10,3,'This is a dummy auction.','User3','Desktops','Computers','F Dummy Desktop 2 - Ten Earths',2005);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (2005,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2005,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2005,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Power Supply','Computers','1');
+VALUES (2005,'Power Supply','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,4,NOW()),10,4,'This is a dummy auction.','User2','Tablets','Computers','B Dummy Tablet 2 - Bubble Gum Tastes Good');
-VALUES (2001,'Processor','Computers','1');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,4,NOW()),10,4,'This is a dummy auction.','User2','Tablets','Computers','B Dummy Tablet 2 - Bubble Gum Tastes Good',2006);
+VALUES (2006,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2006,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2006,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Screen Size','Computers','1');
+VALUES (2006,'Screen Size','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,2,NOW()),10,2,'This is a dummy auction.','User3','Laptops','Computers','Dummy Laptop 3 - Give Us Max Points');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,2,NOW()),10,2,'This is a dummy auction.','User3','Laptops','Computers','Dummy Laptop 3 - Give Us Max Points',2007);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (2007,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2007,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2007,'Memory','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,2008)
 VALUES (TIMESTAMPADD(WEEK,6,NOW()),10,6,'This is a dummy auction.','User2','Desktops','Computers','C Dummy Desktop 3 - We Are Good');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (2008,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2008,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2008,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Power Supply','Computers','1');
+VALUES (2008,'Power Supply','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
-INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title)
-VALUES (TIMESTAMPADD(WEEK,9,NOW()),10,9,'This is a dummy auction.','User1','Tablets','Computers','E Dummy Tablet 3 - You Are Safe');
-VALUES (2001,'Processor','Computers','1');
+START TRANSACTION;
+INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
+VALUES (TIMESTAMPADD(WEEK,9,NOW()),10,9,'This is a dummy auction.','User1','Tablets','Computers','E Dummy Tablet 3 - You Are Safe',2009);
+VALUES (2009,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (2009,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (2009,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Screen Size','Computers','1');
+VALUES (2009,'Screen Size','Computers','1');
 COMMIT;
--- database/oneMinuteAuctions.sql
+++ database/oneMinuteAuctions.sql
@@ -1,30 +1,30 @@
 USE BuyMe;
 
-CREATE TRANSACTION;
+START TRANSACTION;
 INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
 VALUES (TIMESTAMPADD(MINUTE,1,NOW()),60,20,'This is a dummy auction.','User1','Laptops','Computers','One Minute Laptop - One Bid - No Minimum',1001);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (1001,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (1001,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (1001,'Memory','Computers','1');
 COMMIT;
 
 INSERT INTO Bid (amount,bidder,auction)
 VALUES (62.32,'User2',1001);
 
-CREATE TRANSACTION;
+START TRANSACTION;
 INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title, minimumPrice,id)
 VALUES (TIMESTAMPADD(MINUTE,1,NOW()),55,7,'This is a dummy auction.','User2','Desktops','Computers','One Minute Desktop - Two Bids - $80 Minimum',80,1002);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (1002,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (1002,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (1002,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Power Supply','Computers','1');
+VALUES (1002,'Power Supply','Computers','1');
 COMMIT;
 
 INSERT INTO Bid (amount,bidder,auction)
@@ -33,30 +33,30 @@ VALUES (55,'User1',1002);
 INSERT INTO Bid (amount,bidder,auction)
 VALUES (81.3,'User3',1002);
 
-CREATE TRANSACTION;
+START TRANSACTION;
 INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id)
 VALUES (TIMESTAMPADD(MINUTE,1,NOW()),51,5,'This is a dummy auction.','User1','Tablets','Computers','One Minute Tablet - No Bids - No Minimum',1003);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (1003,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (1003,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (1003,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Screen Size','Computers','1');
+VALUES (1003,'Screen Size','Computers','1');
 COMMIT;
 
-CREATE TRANSACTION;
+START TRANSACTION;
 INSERT INTO Auction (closeTime,initialPrice,bidIncrement,description,auctioneer,subcategory,category,title,id,minimumPrice)
 VALUES (TIMESTAMPADD(MINUTE,1,NOW()),70,5,'This is a dummy auction.','User3','Tablets','Computers','One Minute Tablet - 5 Bids - $100 Minimum',1004,100);
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Processor','Computers','1');
+VALUES (1004,'Processor','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'GPU','Computers','1');
+VALUES (1004,'GPU','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Memory','Computers','1');
+VALUES (1004,'Memory','Computers','1');
 INSERT INTO AuctionField (auction,field,category,value)
-VALUES (2001,'Screen Size','Computers','1');
+VALUES (1004,'Screen Size','Computers','1');
 COMMIT;
 
 INSERT INTO Bid (amount,bidder,auction)
